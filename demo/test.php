56b5f735d45dd8c06d91dbfd0ea916cb94311635
bismark <ansahbismark40@gmail.com>
2022-12-14 02:48:37 +0800

    game120

--- demo/test.php
+++ demo/test.php
@@ -1,5 +1,12 @@
 <?php
-
-echo mt_rand(00000,99999);
+// $results = [];
+// for($i=0;$i<5;$i++)
+// {
+//     array_push($results, random_int(0,9));
+// }
+// echo json_encode($results);
+$data = file_get_contents("php://input");
+$data = json_decode($data, true);
+echo json_encode([$data['data']])
 ?>
 
